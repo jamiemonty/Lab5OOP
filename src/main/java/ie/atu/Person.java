6e447f28ca6f485bc5c81a276626cb63c385570a
Jamie <G00419525@atu.ie>
2023-10-23 15:34:39 +0100

    Added new file called Person

--- src/main/java/ie/atu/Person.java
+++ src/main/java/ie/atu/Person.java
@@ -4,4 +4,8 @@ public class Person {
     string firstName;
     string lastName;
     int age;
+    public void()
+    {
+        System.out.println("First name is " + firstName + " LastName" + lastName + "Age " + age);
+    }
 }
