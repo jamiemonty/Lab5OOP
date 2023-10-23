26165c4706ca2b795b218d75c23cb44fa7a6e9ec
Jamie <G00419525@atu.ie>
2023-10-23 15:35:26 +0100

    Added new method called displayInfo

--- src/main/java/ie/atu/Person.java
+++ src/main/java/ie/atu/Person.java
@@ -3,7 +3,7 @@ package ie.atu;
 public class Person {
     string firstName;
     string lastName;
-    int age;
+    int ag;
     public void()
     {
         System.out.println("First name is " + firstName + " LastName" + lastName + "Age " + age);
