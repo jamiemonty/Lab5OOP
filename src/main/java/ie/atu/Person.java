60e813869c2c5b10a34e67f0e700c156f41e6255
Jamie <G00419525@atu.ie>
2023-10-23 16:43:59 +0100

    Added code to prompt the user to enter their details that will be used in creation of a new Person object

--- src/main/java/ie/atu/Person.java
+++ src/main/java/ie/atu/Person.java
@@ -1,5 +1,6 @@
 package ie.atu;
 
+import java.util.Scanner;
 public class Person {
     String firstName;
     String lastName;
@@ -9,6 +10,20 @@ public class Person {
         this.firstName = firstName;
         this.lastName = lastName;
         this.age = age;
+    }
+
+    public void getUserInput()
+    {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter first name: ");
+        this.firstName = scanner.nextLine();
+
+        System.out.print("Enter last name: ");
+        this.lastName = scanner.nextLine();
+
+        System.out.print("Enter age: ");
+        this.age = scanner.nextLine();
     }
 
     public void displayInfo()
